573d98cdc8dee1c936bfc8e43e003bfcfbef80ca
Lucas Colombo <lucasncolombo@gmail.com>
2022-09-18 22:10:56 -0300

    wip: add oktokit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+import { info } from '@actions/core';
 import { getOctokit } from '@actions/github';
 import { inputs } from './core/input';
 
 const opts = inputs();
 const octokit = getOctokit(opts.token);
 
-console.log(opts)
+info('Getting commits');
+getCommits();
+
+async function getCommits() {
+    const commits = await octokit.paginate(
+        octokit.rest.repos.listCommits,
+        {
+            owner: opts.repo.owner,
+            repo: opts.repo.name,
+            per_page: 100,
+        }
+    )
+
+    console.log(commits);
+}
