bcd4d6474c3c7f2f902a9872672207332e47f298
Lucas Colombo <lucasncolombo@gmail.com>
2022-09-19 23:20:35 -0300

    config: add jest deps and setup

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+    displayName: 'action-release',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'js'],
+};
